346df992f37853144a0cc6b48ccb75c664b4bade
Shrenix <shrenikrungta@gmail.com>
2023-01-20 14:01:52 -0500

    Update streamlit_app.py
    Read a CSV file and display on the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents new healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -6,3 +7,6 @@ streamlit.text('1. Bread Omlette')
 streamlit.text('2. Bulls Eye')
 streamlit.text('3. Boiled Eggs')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+myfruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(myfruitlist)
