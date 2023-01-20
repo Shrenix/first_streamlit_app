94a252cce07cf082ee7d94ea36de89b65be448ae
Shrenix <shrenikrungta@gmail.com>
2023-01-20 14:13:07 -0500

    Update streamlit_app.py
    Removed the default fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ myfruitlist = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 myfruitlist = myfruitlist.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruitselected = streamlit.multiselect("Pick some fruits:", list(myfruitlist.index),['Avocado','Strawberries'])
+fruitselected = streamlit.multiselect("Pick some fruits:", list(myfruitlist.index))
 
 fruitToShow = myfruitlist.loc[fruitselected]
 
